0fb446c14ff94d9e25a1a50d696a48987637bfce
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2023-12-25 02:20:14 +0500

    Update db.js

--- src/app/db/db.js
+++ src/app/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export async function ConnectDB(){
     try{
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL)
         console.log('Connected To DataBase');
     }
     catch(error){
